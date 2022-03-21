daf9414e182ab0b08586d23261386ed23b79c94a
jorgeucano <jorgeucano@gmail.com>
2022-03-21 13:31:46 -0300

    hello web3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Moralis } from 'moralis';
+import { User } from './user.component';
+import { environment } from 'src/environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngDApp';
+  user?: User;
+
+  ngOnInit() {
+    Moralis.start({
+      appId: environment.moralis.appId,
+      serverUrl: environment.moralis.serverUrl
+    })
+    .then(() => console.log('DApps inicializada'))
+    .finally(() => this.setLoggedInUser(Moralis.User.current()))
+  }
+
+  private setLoggedInUser(loggedInUser?: User) {
+    console.log(`user`, loggedInUser);
+    this.user = loggedInUser;
+  }
+
+  async login(provider: 'metamask' | 'walletconnect' = 'metamask') {
+    const user = await Moralis.authenticate({provider});
+    this.setLoggedInUser(user);
+  }
+
+  logout() {
+    Moralis.User.logOut()
+    .then((loggedOutUser) => console.log(`Usuario deslogueado`, loggedOutUser))
+    .then(() => this.setLoggedInUser(undefined))
+    .then(() => Moralis.cleanup())
+    .catch((e) => console.error('Moralis logout error:', e));
+  }
+
+
+
 }
